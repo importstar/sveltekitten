f25d3dcbec1f2b4a89823266f4af3a28448adc96
jakee_ind <jakee_ind@hotmail.com>
2025-03-18 22:08:16 +0700

    feat: 🎸 style example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="wrapper">
+	<h1 class="title">SvelteKitten</h1>
+  <a href="/pokemons" class="btn btn-primary mt-5">
+    Pokemon Example
+  </a>
+</div>
+
+<style lang="postcss">
+	@reference "tailwindcss/theme";
+	@plugin "daisyui";
+	.wrapper {
+		@apply flex w-full flex-col items-center justify-center px-4 py-2 h-screen;
+	}
+
+	.title {
+		@apply from-primary bg-gradient-to-r from-5% to-pink-500 to-40% bg-clip-text text-6xl font-bold text-transparent mx-auto w-fit;
+	}
+</style>

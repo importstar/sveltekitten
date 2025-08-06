f494f2fb15cfc9be71ea9603815c4bea095fe5a9
jakee_ind <jakee_ind@hotmail.com>
2025-08-07 01:15:26 +0700

    wip: pre implement api client factory

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,30 +1,4 @@
-export type ApiFetcher = {
-	fetch: typeof fetch;
-	accessToken?: string | null;
-};
+// API Client Factory
 
-export async function apiFetch<T>(
-	url: string,
-	options: RequestInit = {},
-	fetcher: ApiFetcher
-): Promise<T> {
-	if (!fetcher.fetch) {
-		throw new Error('Fetch function is not provided');
-	}
-
-	const headers: HeadersInit = {
-		'Content-Type': 'application/json',
-		...options.headers
-	};
-
-	const response = await fetcher.fetch(url, {
-		...options,
-		headers
-	});
-
-	if (!response.ok) {
-		throw new Error(`API request failed with status ${response.status}`);
-	}
-
-	return (await response.json()) as T;
+export function createApiClient(fetchInstance: typeof fetch) {
 }
